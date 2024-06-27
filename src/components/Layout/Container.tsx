51e13e15f27949602a9a5023b69814078a548ec3
theo.kim <theo.kim@stagefive.com>
2024-06-27 10:51:29 +0900

    feat: 기본 레이아웃 및 폰트 적용

---
+++ src/components/Layout/Container.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Header from './Header/Header';
+import Sidebar from './Sidebar/Sidebar';
+
+const Container = ({ children }: { children: React.ReactNode }) => {
+  // useState
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="flex h-screen overflow-hidden dark:bg-boxdark-2 dark:text-bodydark font-pretendard">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <main>
+          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Container;
